4ab3be3c27a46aa176e660185984a214b2f62871
LikhithareddyM12 <likhithareddymuthumula@gmail.com>
2024-08-03 16:58:48 +0530

    MODIFIED

--- src/javaprogram/rangeDataType.java
+++ src/javaprogram/rangeDataType.java
@@ -5,9 +5,9 @@ public class rangeDataType {
 	
 
 	public static void main(String[] args) {
-		System.out.println(Short.SIZE/8);
+		System.out.println(Short.SIZE/8);//size of data type
 		System.out.println(Byte.SIZE/8);
-		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MIN_VALUE); //Range of Data types
 		System.out.println(Byte.MAX_VALUE);
 		System.out.println(Short.MIN_VALUE);
 		System.out.println(Short.MAX_VALUE);

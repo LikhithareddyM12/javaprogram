82ef9e8fc5bc748f2f599d581df2fd39fde7204e
LikhithareddyM12 <likhithareddymuthumula@gmail.com>
2024-08-04 12:28:33 +0530

    modified

---
+++ src/javaprogram/dataType.java
@@ -0,0 +1,29 @@
+package javaprogram;
+
+public class dataType {
+
+	public static void main(String[] args) {
+
+		byte b  =20;
+		short s =123;
+		long l = 987654321;
+		float f = 3.8f;
+		double d = 34.900;
+		int i =1000;
+		boolean b1 = true;
+		boolean b2 = false;
+		char  c = 'p';
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(i);
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(c);
+		
+
+	}
+
+}
